ccf5c55e92ef66bebb9f5f8e4f64a2667fdf26be
AttakDoge <liambragg1@gmail.com>
2022-07-14 16:38:11 -0400

    Update banned.lua

--- banned.lua
+++ banned.lua
@@ -1,10 +1,10 @@
 --Todo: obfuscate
 local userids = {
-    [3680198138] = true
+    --[3680198138] = true          User: amoamogus999
 }
 
 
-if userids[game.Players.LocalPlayer.UserId] = true then
+if userids[game.Players.LocalPlayer.UserId] then
     game:GetService('StarterGui'):SetCore('SendNotification', {Title = 'DogeHub', Text = 'L bozo + ratio ur banned'})
     wait(0.5)
     game:GetService('StarterGui'):SetCore('SendNotification', {Title = 'DogeHub', Text = 'L bozo + ratio ur banned'})
